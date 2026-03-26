0008f667db90b565f68ac41353d6e637e5766da6
Emeric99 <emerictcholagheu@gmail.com>
2026-03-26 12:46:56 +0100

    Add hello.sh script for Bash basics

---
+++ hello.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Eine Variable definieren
+NAME="Emeric"
+
+# Text ausgeben
+echo "Hallo, $NAME!"
+echo "Willkommen bei Bash for Beginners."
+
+# Mehrere Zeilen ausgeben
+echo ""
+echo "Heute lernst du:"
+echo "  - Was ein Bash-Skript ist"
+echo "  - Wie man Variablen benutzt"
+echo "  - Wie man Text ausgibt"
+
+# Datum anzeigen
+DATUM=$(date +"%d.%m.%Y")
+echo ""
+echo "Heutiges Datum: $DATUM"
